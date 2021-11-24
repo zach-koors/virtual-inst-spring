fe0cb22c30f1e0a9c8dc8cacb81ff11c0f7f574b
Zach Koors <koorsz@gmail.com>
2021-11-24 08:49:43 -0500

    created SequencerState domain class

---
+++ src/main/java/com/zachkoors/virtualinstspring/domain/SequencerState.java
@@ -0,0 +1,22 @@
+package com.zachkoors.virtualinstspring.domain;
+
+public class SequencerState {
+    private String[] stepClasses;
+    private String sequencerStateName;
+
+    public String getSequencerStateName() {
+        return sequencerStateName;
+    }
+
+    public void setSequencerStateName(String sequencerStateName) {
+        this.sequencerStateName = sequencerStateName;
+    }
+
+    public String[] getStepClasses() {
+        return stepClasses;
+    }
+
+    public void setStepClasses(String[] stepClasses) {
+        this.stepClasses = stepClasses;
+    }
+}
