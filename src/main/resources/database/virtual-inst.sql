c3bd1f63b6a327845ffbd4943ae70d13aba9a805
Zach Koors <koorsz@gmail.com>
2021-11-24 12:35:06 -0500

    added jdbc dependency, modified sql schema

--- src/main/resources/database/virtual-inst.sql
+++ src/main/resources/database/virtual-inst.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS sequencer_state;
 
 CREATE TABLE sequencer_state
 (
-    sequencer_state_name varchar(16),
-    step_classes varchar(max)
+    sequencer_state_name text,
+    step_classes text[],
     PRIMARY KEY (sequencer_state_name)
 );
