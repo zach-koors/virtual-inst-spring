b8ded8794bd26b9fcd5802f382bce56be73acc08
Zach Koors <koorsz@gmail.com>
2021-11-24 08:41:05 -0500

    initial commit

---
+++ src/main/java/com/zachkoors/virtualinstspring/VirtualInstSpringApplication.java
@@ -0,0 +1,13 @@
+package com.zachkoors.virtualinstspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualInstSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualInstSpringApplication.class, args);
+	}
+
+}
