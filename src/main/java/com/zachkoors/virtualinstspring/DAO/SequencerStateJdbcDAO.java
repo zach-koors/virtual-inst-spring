c76bd3526021efa1f866075a7e3fb70cebef11c6
Zach Koors <koorsz@gmail.com>
2021-12-03 10:45:43 -0500

    wrote JdbcDAO method for retrieveSequencerState, yet to test

--- src/main/java/com/zachkoors/virtualinstspring/DAO/SequencerStateJdbcDAO.java
+++ src/main/java/com/zachkoors/virtualinstspring/DAO/SequencerStateJdbcDAO.java
@@ -3,6 +3,7 @@ package com.zachkoors.virtualinstspring.DAO;
 import com.zachkoors.virtualinstspring.domain.SequencerState;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -14,7 +15,7 @@ public class SequencerStateJdbcDAO implements SequencerStateDAO{
 
     @Override
     public SequencerState saveSequencerState(SequencerState sequencerState) {
-        String sql = "INSERT INTO sequencer_state (sequencer_state_name, step_classes) " +
+        String sql = "INSERT INTO sequencer_state (sequencer_state_name, step_classes) \n" +
                 "VALUES (?, ?)";
         String beatName = sequencerState.getSequencerStateName();
         String[] stepClasses = sequencerState.getStepClasses();
@@ -24,8 +25,15 @@ public class SequencerStateJdbcDAO implements SequencerStateDAO{
 
     @Override
     public SequencerState retrieveSequencerState(String sequencerStateName) {
-
-        return null;
+        SequencerState sequencerState = null;
+        String sql = "SELECT * \n" +
+                "FROM sequencer_state \n" +
+                "WHERE sequencer_state_name = ?";
+        SqlRowSet result = jdbcTemplate.queryForRowSet(sql, sequencerStateName);
+        if (result.next()){
+            sequencerState = new SequencerState(result.getString(0), result.getObject(1, String[].class));
+        }
+        return sequencerState;
     }
 
     @Override
